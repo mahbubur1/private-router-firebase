dbd9771569c5dcbec84fb69df25b7cac0182b5a7
mahbubur1 <mdmahbuborr462@gail.com>
2022-04-16 01:12:40 +0600

    privat route & protected route set

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,7 @@
 import { getAuth } from "firebase/auth";
 import React from "react";
 import {useSignInWithGoogle} from "react-firebase-hooks/auth"
+import { useLocation, useNavigate } from "react-router-dom";
 import app from "../../firebase.init";
 
 // import useFirebase from "../../hooks/useFirebase";
@@ -8,12 +9,23 @@ const auth = getAuth(app);
 const Login = () => {
   // const {singInWithGoogle} = useFirebase();
   const [signInWithGoogle,user] = useSignInWithGoogle(auth);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const from = location?.state?.from?.pathname || "/";
+
+  const handleSignWithGoogle = () => {
+    signInWithGoogle()
+    .then(() => {
+      navigate(from,{ replace: true });
+    })
+  }
   return (
     <div>
       <h2>Please log in</h2>
       <div style={{marginBottom: '20px'}}>
         {/* singInWithGoogle function call in button */}
-        <button onClick={() =>signInWithGoogle()}>Google Sing in</button>
+        <button onClick={handleSignWithGoogle}>Google Sing in</button>
       </div>
 
       <form action="">
